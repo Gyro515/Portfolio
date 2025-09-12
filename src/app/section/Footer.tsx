49c8d415f3c0b91be4ebb84f3f1a05672b8f341c
Justin <jsagebriones@yahoo.com>
2025-09-12 18:40:04 -0500

    Update footer and header

--- src/app/section/Footer.tsx
+++ src/app/section/Footer.tsx
@@ -6,8 +6,8 @@ import {TbBrandTypescript, TbBrandNextjs, TbBrandTailwind, TbBrandVercel, TbBran
 export default function Footer() {
     return (
         <footer className="w-full bg-white shadow-[0_-4px_6px_-1px_rgba(0,0,0,0.2)] border-t border-gray-200 relative z-50">
-            <div className="mx-auto max-w-screen-xl px-5 py-4">
-                <div className="flex justify-between items-center">
+            <div className="mx-auto px-5 py-4">
+                <div className="flex flex-col justify-between items-center">
 
                     {/* Social Media */}
                     <div>
