873f552681969a2ee7e92a1e3df770fa85a5dd26
Justin <jsagebriones@yahoo.com>
2025-09-12 18:41:36 -0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,28 +21,6 @@ function ScrollLoad(){
   return null;
 }
 
-function TechStackComponent({
-  category,
-  tech = [],
-  size = 64,
-}:{
-  category: string;
-  tech?: string[];
-  size?: number;
-}){
-  return(
-    <article className="flex flex-row gap-8 items-center p-2">
-      <div className="font-extrabold text-gray-800">{category}</div>
-      <ul className="ml-auto flex flex-wrap items-center gap-4 justify-end">
-        {tech.map((t, idx) => (
-          <li key={idx}>
-            <Icon icon={t} width={size} height={size} />
-            </li>
-        ))}
-      </ul>
-    </article>
-  )
-}
 
 function ProjectCardComponent({
   name,
@@ -107,37 +85,6 @@ export default function Home() {
             </motion.h2>
           </section>
 
-          {/* Technologies */}
-          <section id="technologies" className="scroll-mt-60 flex flex-col md:flex-row flex-wrap justify-center mx-auto gap-8 px-16 pt-16 pb-16">
-            <div className="flex flex-col justify-center md:col-span-1 max-w-lg">
-              <h1 className="text-7xl font-extrabold italic uppercase text-gray-900">What <br/> I built with</h1>
-              <h2 className="pt-10 text-lg font-semibold leading-relaxed text-gray-600">
-                Over the years I&apos;ve worked with cutting-edge technologies, making interfaces easy to use, connecting the pieces behind the scenes and bringing it to life. </h2>
-            </div>
-
-            <div className="pt-1 flex flex-col text-left justify-center pl-10 border-l-2 border-gray-900"> 
-              <TechStackComponent
-              category="Programming:"
-              tech={["devicon:java", "devicon:javascript", "devicon:typescript", "devicon:python", "devicon:r", "devicon:csharp"]}
-              />
-              <TechStackComponent
-              category="Frameworks & Libraries:"
-              tech={["devicon:react", "devicon:nextjs", "devicon:nodejs", "devicon:express"]}
-              />
-              <TechStackComponent
-              category="Database & Cloud:"
-              tech={["devicon:postgresql", "devicon:mysql", "devicon:googlecloud", "devicon:azure"]}
-              />
-              <TechStackComponent
-              category="Analytics & Visualization:"
-              tech={["logos:tableau-icon", "devicon:pandas", "devicon:numpy", "vscode-icons:file-type-excel"]}
-              />
-              <TechStackComponent
-              category="Tools:"
-              tech={["devicon:git", "devicon:github", "devicon:vscode", "devicon:postman", "devicon:figma"]}
-              />
-            </div>
-          </section>
 
           {/* Project Section */}
           <section id="projects" className="scroll-mt-36 flex flex-col lg:flex-row items-center gap-16 xl:gap-28 mx-auto py-16 md:py-28">
