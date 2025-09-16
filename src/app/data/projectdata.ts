8180aab43a101dea39278275a4f70a1c3db1a8d2
Justin <jsagebriones@yahoo.com>
2025-09-16 11:59:57 -0500

    Update location of images and content of projectdata

--- src/app/data/projectdata.ts
+++ src/app/data/projectdata.ts
@@ -1,23 +1,20 @@
-import cms from "../projects/cms.png";
-import mf from "../projects/mf.png";
-
-export const projectdata = [
+export const projectData = [
     {
         name: "Client Management System",
         description: "End-to-end client management system that includes a client portal and admin dashboard.",
-        image: cms,
+        image: "/cms.png",
         tech: ["Node.js", "Express.js", "PostgreSQL", "Auth0", "SendGrid", "Bootstrap"]
     },
     {
         name: "Moneywise Financial",
         description: "Security-focused architecture case study for a hypothetical banking platform.",
-        image: mf,
+        image: "/mf.png",
         tech: []
     },
         {
         name: "Client s Sdystem",
         description: "End-to-end client management system that includes a client portal and admin dashboard.",
-        image: cms,
+        image: "/cms.png",
         tech: ["Node.js", "Express.js", "PostgreSQL", "Auth0", "SendGrid", "Bootstrap"]
     },
 ];
